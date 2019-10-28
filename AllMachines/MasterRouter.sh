2241cec4bb6b549bf92e6e9deadf3b7f08b2856a
MajkelMA <50554203+MajkelMA@users.noreply.github.com>
2019-10-28 16:43:59 +0100

    Update MasterRouter.sh

--- AllMachines/MasterRouter.sh
+++ AllMachines/MasterRouter.sh
@@ -1,2 +1,10 @@
 sudo ip route add 10.0.1.0/24 via 192.168.1.3
 sudo ip route add 10.0.2.0/24 via 192.168.1.3
+sudo yum -y install tcpdump
+sudo yum -y install ipvsadm
+sudo touch /etc/sysconfig/ipvsadm
+sudo systemctl start ipvsadm
+sudo systemctl enable ipvsadm
+sudo ipvsadm -C
+sudo ipvsadm -A -t 192.168.1.1:80 -s wrr
+sudo ipvsadm -a -t 192.168.1.1:80 -r 10.0.0.101:80 -w 10 -m
--- AllMachines/ServersScript/installHttpd.sh
+++ AllMachines/ServersScript/installHttpd.sh
@@ -1,15 +1,15 @@
-yum install httpd -y
-systemctl enable httpd 
-systemctl start httpd
+sudo yum install httpd -y
+sudo systemctl enable httpd 
+sudo systemctl start httpd
 sudo mkdir -p /var/www/page.ex1.gr4/html
 sudo chmod -R 775 /var/www
 $page = "<html><head><title>Zad 1</title><body>Strona na {$1}</body></head> </html>"
 echo "$page" > /var/www/page.ex1.gr4/html/index.html
-sudo mkdir /etc/httpd/sites
+sudo sudo mkdir /etc/httpd/sites
 echo "IncludeOptional sites/*.conf" >> /etc/httpd/conf/httpd.conf
 echo "<VirtualHost *:80>
     ServerName www.server1.ex1.gr4
     ServerAlias server1.ex1.gr4
     DocumentRoot /var/www/page.ex1.gr4/html
 </VirtualHost>" > /etc/httpd/sites/server1.ex1.gr4.conf
-systemctl restart httpd
+sudo systemctl restart httpd
