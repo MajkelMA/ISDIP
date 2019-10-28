a5fdf3f5bb55d51bdf335e937f1f1c4e8462f31d
MajkelMA <50554203+MajkelMA@users.noreply.github.com>
2019-10-28 17:46:35 +0100

    Update AllMachines

--- AllMachines/ServersScript/installHttpd.sh
+++ AllMachines/ServersScript/installHttpd.sh
@@ -3,7 +3,7 @@ sudo systemctl enable httpd
 sudo systemctl start httpd
 sudo mkdir -p /var/www/page.ex1.gr4/html
 sudo chmod -R 775 /var/www
-$page = "<html><head><title>Zad 1</title><body>Strona na {$1}</body></head> </html>"
+page="<html><head><title>Zad 1</title><body>Strona na {$1}</body></head> </html>"
 echo "$page" > /var/www/page.ex1.gr4/html/index.html
 sudo sudo mkdir /etc/httpd/sites
 echo "IncludeOptional sites/*.conf" >> /etc/httpd/conf/httpd.conf
