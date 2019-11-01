ff6be713dfa05d3da0c2cad9fc0ad39ac40770f0
MajkelMA <50554203+MajkelMA@users.noreply.github.com>
2019-11-01 21:59:23 +0100

    Update all files

--- AllMachines/load-balancer-scripts/master-router-up.sh
+++ AllMachines/load-balancer-scripts/master-router-up.sh
@@ -1,4 +1,4 @@
-sudo systemctl start firewalld
+# sudo systemctl stop firewalld
 sudo ip addr add 192.168.1.100/24 broadcast 192.168.1.255 dev eth1 label eth1:1
 sudo ip addr add 10.0.0.100/24 broadcast 10.0.0.255 dev eth2 label eth2:1
 # sudo ipvsadm -C
@@ -6,4 +6,3 @@ sudo ip addr add 10.0.0.100/24 broadcast 10.0.0.255 dev eth2 label eth2:1
 # sudo ipvsadm -a -t 192.168.1.100:80 -r 10.0.0.101:80 -w 10 -m
 # sudo ipvsadm -a -t 192.168.1.100:80 -r 10.0.0.102:80 -w 20 -m
 # sudo ipvsadm -a -t 192.168.1.100:80 -r 10.0.0.103:80 -w 10 -m
-sudo systemctl stop firewalld
--- AllMachines/router-scripts/client-router-conf.sh
+++ AllMachines/router-scripts/client-router-conf.sh
@@ -1,5 +1,6 @@
     yum -y install dnf
     dnf -y upgrade procps
+    yum -y install httpd-tools
     sudo echo 'net.ipv4.ip_forward = 1' >> /etc/sysctl.conf
     sysctl -p
     sudo systemctl enable firewalld
