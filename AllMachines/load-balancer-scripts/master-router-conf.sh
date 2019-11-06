6812e1f633b3aeff85f012b796e9e016ac3665bc
MajkelMA <50554203+MajkelMA@users.noreply.github.com>
2019-11-06 20:04:35 +0100

    Try to fix firewall (not work - it has to be switched off on load balancers)

--- AllMachines/load-balancer-scripts/master-router-conf.sh
+++ AllMachines/load-balancer-scripts/master-router-conf.sh
@@ -21,9 +21,14 @@ sudo systemctl disable NetworkManager
 
 # konfiguracja keepalived
 sudo yum -y install keepalived
-# sudo echo 'net.ipv4.ip_nonlocal_bind = 1' >> /etc/sysctl.conf
 sudo sysctl -p 
 sudo mv /etc/keepalived/keepalived.conf /etc/keepalived/keepalived.conf.org
 sudo cat /vagrant_data/keepalived-master-conf.txt > /etc/keepalived/keepalived.conf
 sudo systemctl start keepalived
 sudo systemctl enable keepalived
+
+# firewall-cmd --direct  --add-rule ipv4 filter INPUT 0 --in-interface eth1 --destination 224.0.0.18 --protocol vrrp -j ACCEPT
+# firewall-cmd --direct --add-rule ipv4 filter OUTPUT 0 --out-interface eth1 --destination 224.0.0.18 --protocol vrrp -j ACCEPT
+
+# firewall-cmd --direct  --add-rule ipv4 filter INPUT 0 --in-interface eth2 --destination 224.0.0.18 --protocol vrrp -j ACCEPT
+# firewall-cmd --direct --add-rule ipv4 filter OUTPUT 0 --out-interface eth2 --destination 224.0.0.18 --protocol vrrp -j ACCEPT
--- AllMachines/load-balancer-scripts/slave-router-conf.sh
+++ AllMachines/load-balancer-scripts/slave-router-conf.sh
@@ -13,3 +13,9 @@ sudo mv /etc/keepalived/keepalived.conf /etc/keepalived/keepalived.conf.org
 sudo cat /vagrant_data/keepalived-slave-conf.txt > /etc/keepalived/keepalived.conf
 sudo systemctl start keepalived
 sudo systemctl enable keepalived
+
+# firewall-cmd --direct  --add-rule ipv4 filter INPUT 0 --in-interface eth1 --destination 224.0.0.18 --protocol vrrp -j ACCEPT
+# firewall-cmd --direct --add-rule ipv4 filter OUTPUT 0 --out-interface eth1 --destination 224.0.0.18 --protocol vrrp -j ACCEPT
+
+# firewall-cmd --direct  --add-rule ipv4 filter INPUT 0 --in-interface eth2 --destination 224.0.0.18 --protocol vrrp -j ACCEPT
+# firewall-cmd --direct --add-rule ipv4 filter OUTPUT 0 --out-interface eth2 --destination 224.0.0.18 --protocol vrrp -j ACCEPT
