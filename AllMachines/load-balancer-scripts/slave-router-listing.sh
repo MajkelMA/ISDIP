7ff01243dfcef7135de5ede06a0d7954a0125699
Marcin <bee4004@gmail.com>
2019-11-09 23:37:48 +0100

    VRRP added

---
+++ AllMachines/load-balancer-scripts/slave-router-listing.sh
@@ -0,0 +1,3 @@
+sudo sysctl -p > /vagrant_data/7-task-MM/konfiguracja-jako-router.txt
+sudo ip a > /vagrant_data/7-task-MM/adresacja-sieci.txt
+sudo ip r > /vagrant_data/7-task-MM/tablica-routingu.txt
