7d32b2f382d3b34b90f2752ce2ca6ce191e88054
Sorcuss <31709862+Sorcuss@users.noreply.github.com>
2019-11-14 23:11:14 +0100

    some fix

--- AllMachines/load-balancer-scripts/master-router-firewall.sh
+++ AllMachines/load-balancer-scripts/master-router-firewall.sh
@@ -4,8 +4,10 @@ sudo firewall-cmd --zone=public --remove-interface=eth1 --permanent
 sudo firewall-cmd --zone=public --remove-interface=eth2 --permanent
 sudo firewall-cmd --zone=external --add-interface=eth1 --permanent
 sudo firewall-cmd --zone=internal --add-interface=eth2 --permanent
-sudo firewall-cmd --reload
 sudo firewall-cmd --add-masquerade --zone=external --permanent
 sudo firewall-cmd --add-service=http --zone=external --permanent
 sudo firewall-cmd --direct --permanent --add-rule ipv4 filter FORWARD 0 -i eth2 -o eth1 -j ACCEPT 
+sudo firewall-cmd --direct --permanent --add-rule ipv4 filter INPUT 0 -p vrrp -s 192.168.1.0/24 -d 224.0.0.0/24 -j ACCEPT
+sudo firewall-cmd --direct --permanent --add-rule ipv4 filter INPUT 0 -p vrrp -s 10.0.0.0/24 -d 224.0.0.0/24 -j ACCEPT
+sudo firewall-cmd --direct --permanent --add-rule ipv4 filter INPUT 0 -p udp -s 192.168.1.0/25 -d 224.0.0.0/24 -j ACCEPT
 sudo firewall-cmd --reload
